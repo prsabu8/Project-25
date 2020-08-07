1ef357bb0eebb929aa484cb773476fffb3725052
prsabu8 <68687182+prsabu8@users.noreply.github.com>
2020-08-07 15:10:12 +0530

    Add files via upload

--- dustbin.js
+++ dustbin.js
@@ -28,30 +28,6 @@ class dustbin
 			var posLeft=this.leftWallBody.position;
 			var posRight=this.rightWallBody.position;
 
-			
-
-			push()
-			translate(posLeft.x, posLeft.y);
-			rectMode(CENTER)
-			//strokeWeight(4);
-			angleMode(RADIANS)
-			fill(255)
-			stroke(255)
-			rotate(this.angle)
-			rect(0,0,this.wallThickness, this.dustbinHeight);
-			pop()
-
-			push()
-			translate(posRight.x, posRight.y);
-			rectMode(CENTER)
-			//strokeWeight(4);
-			stroke(255)
-			angleMode(RADIANS)
-			fill(255)
-			rotate(-1*this.angle)
-			rect(0,0,this.wallThickness, this.dustbinHeight);
-			pop()
-
 			push()
 			translate(posBottom.x, posBottom.y);
 			rectMode(CENTER)
